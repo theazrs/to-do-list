a4fc9c09515eedb6dffe77f641f6f20844241d94
Azure_z <azarias.tomas@gmail.com>
2026-03-16 16:00:08 +0200

    App now works mostly fine, a few tweaks missing.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <string>
+#include <string> // to use getline
 #include <vector> // must include to use vectors 
 using namespace std; 
 
@@ -10,8 +10,7 @@ vector<string> tasks = {};
 void waitOnInput() {
     cout << "Press ENTER to continue... ";
     cin.get();
-    cin.ignore();
-    
+
 }
 
 // main screen
@@ -25,7 +24,7 @@ void index() {
 
 // function to output to screen
 void printTasks() {
-    int i = 0;
+    int i = 1;
     for (string task : tasks) {
         cout << i << ". " << task << endl;
         i++;
@@ -37,53 +36,105 @@ void addTask() {
     string task;
     cout << "Enter the name of the task" << endl;
     getline(cin, task);
-    cin.ignore();
     tasks.push_back(task);
     // clear 
     cout << "Task added successfully" << endl;
 }
 
+// function to mark task as done (temp)
 void deleteTask(int index) {
-    if (tasks.size() < index) {
-        tasks.erase(tasks.begin() + index);
+    if (tasks.size() <= index && index > 0) {
+        tasks.erase(tasks.begin() + index - 1);
         cout << "Task marked as done / deleted" << endl;
     } 
     else {
         cout << "Error!! " << endl;
     }
 }
+
+// function to check if vector is em
+int isEmpty() {
+    if (tasks.empty() == true) {
+        return 1;
+    }
+    return 0;
+}
+
+// function to clear screen
+void clearScreen() {
+    cout << "\033[2J\033[1;1H" << endl;
+}
+
 // main
 int main() {
+    clearScreen();
     int response;
 
     // initial screen
     cout << "Welcome to my to-do list!!!" << endl << endl;
-    index();
-    cin >> response;
-    switch(response) {
-        case 1:
-            cout << "Your tasks: " << endl << endl;
-            printTasks();
-            waitOnInput();
-            break;
+    while (true) {
+        index();
+        cin >> response;
+        cin.ignore();
+        clearScreen();
+        switch(response) {
+            case 1:
+                // if user has no tasks it won't have to print your tasks
+                if (isEmpty()) {
+                    cout << "To-do list is empty, add tasks to be able to view them.." << endl;
+                    waitOnInput();
+                    clearScreen();
+                    break;
+                }
+            
+                // else
+                cout << "Your tasks: " << endl << endl;
+                printTasks();
+                waitOnInput();
+                clearScreen();
+                break;
 
-        case 2:
-            addTask();
-            break;
+            case 2:
+                addTask();
+                waitOnInput();
+                clearScreen();
+                break;
 
-        case 3:
-            // add check to ensure users without any task cannot input anything
-            int response1;
-            printTasks();
-            cin >> response1;
-            // function to delete 
-            break;
+            case 3:
+                // check to ensure users without any task cannot view
+                if (isEmpty()) {
+                    cout << "To-do list is empty, add tasks to be able to mark them as done" << endl;
+                    waitOnInput();
+                    clearScreen();
+                    break;
+                }
 
-        case 4:
-            cout << "Exiting..." << endl;
-            // write to file 
-            return 0;
+                // else
+                int response1;
+                clearScreen();
+                printTasks();
+                cout << "Choose which task to mark as done." << endl;
+                cin >> response1;
+                cin.ignore();
+                deleteTask(response1);
+                waitOnInput();
+                clearScreen();
+                break;
+
+            case 4:
+                cout << "Exiting..." << endl;
+
+                // write to file 
+                return 0;
+
+            default:
+                cout << "Invalid option" << endl;
+                waitOnInput();
+                clearScreen();
+                break;
+        }
     }
+    
     
     return 0;
 }
